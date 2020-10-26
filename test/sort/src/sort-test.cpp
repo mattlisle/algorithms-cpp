a7dd3b8a3a0d80ef2f985277166968d667d337dc
Matt Lisle <matthew.j.lisle@gmail.com>
2020-10-25 17:19:30 -0700

    test: for merge sort

--- test/sort/src/sort-test.cpp
+++ test/sort/src/sort-test.cpp
@@ -1,4 +1,64 @@
-//
-// Created by Konstantin Gredeskoul on 5/16/17.
-//
+#include <sort.hpp>
+#include <limits>
+#include <string>
 #include "gtest/gtest.h"
+
+using namespace std;
+
+class MergeSortTest : public ::testing::Test {
+  protected:
+    vector<int> zero_element {};
+    vector<int> one_element {1};
+    vector<int> positive_elements {1, 5, 2, 9, 3};
+    vector<int> negative_elements {-1, -3, -2, -8, -4};
+    vector<int> mixed_elements {10, -20, -20, 40, -34, -100, 2};
+    vector<int> large_elements {numeric_limits<int>::max(), 0, numeric_limits<int>::min()};
+
+    virtual void SetUp() {
+
+    };
+
+    virtual void TearDown() {
+
+    };
+
+    virtual void test_sorting_of(vector<int>& vec) {
+      MergeSorter sorter {vec};
+      sorter.sort();
+      stringstream message;
+      message << "sorting failed on this subsequence: ";
+      int prev = numeric_limits<int>::min();
+      for (const auto& elem: sorter.result) {
+        message << elem << " ";
+        if (prev > elem) {
+          FAIL() << message.str();
+        } else {
+          prev = elem;
+        }
+      }
+    }
+};
+
+TEST_F(MergeSortTest, GivenZeroElements_Ok) {
+  test_sorting_of(zero_element);
+}
+
+TEST_F(MergeSortTest, GivenOneElement_Ok) {
+  test_sorting_of(one_element);
+}
+
+TEST_F(MergeSortTest, GivenPositiveElements_Ok) {
+  test_sorting_of(positive_elements);
+}
+
+TEST_F(MergeSortTest, GivenNegativeElements_Ok) {
+  test_sorting_of(negative_elements);
+}
+
+TEST_F(MergeSortTest, GivenMixedElements_Ok) {
+  test_sorting_of(mixed_elements);
+}
+
+TEST_F(MergeSortTest, GivenLargeElements_Ok) {
+  test_sorting_of(zero_element);
+}
