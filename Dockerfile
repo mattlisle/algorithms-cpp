42a78af3b576bdd6c7a67d281d59047d333194d9
Matt Lisle <matthew.j.lisle@gmail.com>
2020-11-15 15:58:02 -0800

    feat: formatting during build
    Brittle implementation works... Should have something better than a bash
    script for this.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ RUN apt-get update && apt-get install -y \
   build-essential \
   g++ \
   cmake \
-  clang-tidy-9
+  clang-tidy-9 \
+  clang-format-9
 
 WORKDIR /usr/src
 COPY . .
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,10 +4,23 @@ set -e
 
 WORKDIR=$(pwd)
 
+
 mkdir -p build
 cd build
+
+# Build
 cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 cmake --build .
-python3 ../scripts/run-clang-tidy.py -p $WORKDIR/build files $WORKDIR/src $WORKDIR/test
+
+# Lint
+python3 ../scripts/run-clang-tidy.py -p $WORKDIR/build files $WORKDIR/src $WORKDIR/test $WORKDIR/include/algorithms-cpp
+
+# Format
+! find $WORKDIR/src/ $WORKDIR/test/ $WORKDIR/include/algorithms-cpp -name '*.cpp' -o -name '*.hpp' |
+  xargs clang-format-9 --output-replacements-xml |
+  grep '<replacement ' > /dev/null ||
+  echo "There were format violations" && exit 1
+
+# Test
 ctest
 
