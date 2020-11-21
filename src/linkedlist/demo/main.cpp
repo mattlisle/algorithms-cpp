723969e10d5f0b9479addf35b84931f4ee63d5f5
Matt Lisle <matthew.j.lisle@gmail.com>
2020-11-21 13:30:53 -0800

    feat: linked list implementation

---
+++ src/linkedlist/demo/main.cpp
@@ -0,0 +1,36 @@
+
+#include "linkedlist.hpp"
+#include "util.hpp"
+#include <string>
+
+using namespace std;
+using namespace util;
+
+auto main(int argc, char** argv) -> int {
+
+  string header = "Linked List Demo";
+  string usage = "./linkedlist-demo [numbers ...]";
+  string description = "Constructs a linked list and prints it";
+
+  if (argc == 1) {
+    print_usage(header, usage, description);
+    exit(0);
+  }
+
+  linkedlist::Seq<int> seq{};
+
+  try {
+    for (int i = 1; i < argc; ++i) {
+      int int_arg = stoi(argv[i], nullptr, 0); // NOLINT
+      seq.prepend(int_arg);
+    }
+  } catch (const exception& ex) {
+    cout << "Invalid argument provided, see usage below:" << endl;
+    print_usage(header, usage, description);
+    exit(1);
+  }
+
+  cout << seq << endl;
+
+  return 0;
+}
---
+++ test/linkedlist/src/linkedlist-test.cpp
@@ -0,0 +1,23 @@
+#include "linkedlist.hpp"
+#include "gtest/gtest.h"
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <string>
+
+class LinkedListTest : public ::testing::Test {
+public:
+  void SetUp() override{
+
+  };
+
+  void TearDown() override{
+
+  };
+};
+
+TEST_F(LinkedListTest, sandbox) { // NOLINT
+  linkedlist::Seq<int> seq{};
+  seq.prepend(1);
+  std::cout << seq.head();
+}
